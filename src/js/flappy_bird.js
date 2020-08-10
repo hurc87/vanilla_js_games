9e416b122d65f3251d3fe83ea477ddad48fab0bd
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-08-10 17:21:31 +0100

    game functionality complete

--- src/js/flappy_bird.js
+++ src/js/flappy_bird.js
@@ -1,8 +1,3 @@
-// * There are enemy blocks which move towards the character.
-// * The character is not allowed to touch the enemy blocks.
-// * The enemy blocks change heigh as on each turn
-// * The character scores a point for each successful time they manage to jump through the enemy blocks. 
-// * The game must finish and animations stop if the character hits either the block or ground with the score being displayed with a 'game over' message. 
 // * The user can press a button to restart the game.
 // * The user can select a theme which will change the character / background / ground / enemy blocks 
 
@@ -11,22 +6,39 @@ const startGame = () => {
     const character = document.getElementById('character')
     const enemy_1 = document.getElementById('enemy_block_1')
     const enemy_2 = document.getElementById('enemy_block_2')
+    let score = 0
 
     const gamePlay = setInterval(() => {
         const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'))
         const characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue('bottom'))
 
         const enemy_1_left = parseInt(window.getComputedStyle(enemy_1).getPropertyValue('left'))
-        const enemy_1_top = parseInt(window.getComputedStyle(enemy_1).getPropertyValue('top'))
+        let enemy_1_top = parseInt(window.getComputedStyle(enemy_1).getPropertyValue('top'))
 
-        const enemy_2_bottom = parseInt(window.getComputedStyle(enemy_2).getPropertyValue('bottom'))
+        let enemy_2_bottom = parseInt(window.getComputedStyle(enemy_2).getPropertyValue('bottom'))
 
         character.style.top = characterTop + 3 + 'px'
         enemy_1.style.left = enemy_1_left - 3 + 'px'
         enemy_2.style.left = enemy_1_left - 3 + 'px'
 
-        if ((characterBottom < 85) || ( ( enemy_1_left < 190 && enemy_1_left > 120 ) && ( ( ( (characterBottom + 35) > enemy_2_bottom ) || ( characterBottom < enemy_1_top  ) ) && (enemy_1_left < 189 ) ) )) {
+        if ((characterBottom < 85) || ( ( enemy_1_left < 190 && enemy_1_left > 120 ) && ( ( ( (characterBottom + 35) > enemy_2_bottom ) || ( (characterTop + 35) > enemy_1_top  ) ) ) )) {
             gameOver()
+        }
+
+        // Moves enemy block back to the beginning if it passes successfully all the way to the left
+        // also sets the enemy top / bottom variables to the latest version of the enemy block
+        if (enemy_1_left < -40 ) {
+            enemy_1.style.left = 600 + 'px'
+            const heightGenerator = Math.floor(Math.random() * 251) -150
+
+            enemy_1.style.top = 250 + heightGenerator
+            enemy_2.style.top = -250 + heightGenerator
+
+            enemy_2_bottom = parseInt(window.getComputedStyle(enemy_2).getPropertyValue('bottom'))
+            enemy_1_top = parseInt(window.getComputedStyle(enemy_1).getPropertyValue('top'))
+
+            score ++
+            
         }
 
         const jump = () => {
@@ -40,10 +52,11 @@ const startGame = () => {
                 jump()
             }
         })
+
     }, 100)
 
     const gameOver = () => {
-        console.log('Game over')
+        alert('Game Over! Your score is: ' + score + '! Refresh your page to play again')
         clearInterval(gamePlay)
     }
 }

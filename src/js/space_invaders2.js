27c71f864b22d2de118e9e4e4c44ce3c0e973783
Zoe Hurcombe-straker <37640287+hurc87@users.noreply.github.com>
2020-08-25 15:32:19 +0100

    enemys move

--- src/js/space_invaders2.js
+++ src/js/space_invaders2.js
@@ -22,6 +22,83 @@ const startGame = () => {
     const createHero = document.getElementById(104)
     createHero.classList.add('hero')
 
+    let enemyPosition1 = [1, 3, 5, 7, 9, 13, 15, 17, 19]
+    let enemyPosition2 = enemyPosition1.map(item => { return item - 1 })
+    let enemyPosition3 = enemyPosition2.map(item => { return item + 11 })
+    let enemyPosition4 = enemyPosition3.map(item => { return item + 1 })
+    let enemyPosition5 = enemyPosition4.map(item => { return item + 1 })
+    let enemyPosition6 = enemyPosition5.map(item => { return item + 11 })
+    let enemyPosition7 = enemyPosition6.map(item => { return item - 1 })
+    let enemyPosition8 = enemyPosition7.map(item => { return item - 1 })
+    let enemyPosition9 = enemyPosition8.map(item => { return item + 11 })
+    let enemyPosition10 = enemyPosition9.map(item => { return item + 1 })
+    let enemyPosition11 = enemyPosition10.map(item => { return item + 1 })
+    let enemyPosition12 = enemyPosition11.map(item => { return item + 11 })
+    let enemyPosition13 = enemyPosition12.map(item => { return item - 1 })
+    let enemyPosition14 = enemyPosition13.map(item => { return item - 1 })
+    let enemyPosition15 = enemyPosition14.map(item => { return item + 11 })
+    let enemyPosition16 = enemyPosition15.map(item => { return item + 1 })
+    let enemyPosition17 = enemyPosition16.map(item => { return item + 1 })
+
+    let enemyPosition18 = enemyPosition17.map(item => { return item + 11 })
+    let enemyPosition19 = enemyPosition18.map(item => { return item - 1 })
+    let enemyPosition20 = enemyPosition19.map(item => { return item - 1 })
+
+    let enemyPosition21 = enemyPosition20.map(item => { return item + 11 })
+    let enemyPosition22 = enemyPosition21.map(item => { return item + 1 })
+    let enemyPosition23 = enemyPosition22.map(item => { return item + 1 })
+    let enemyPosition24 = enemyPosition23.map(item => { return item + 11 })
+
+    const enemyMoves = [enemyPosition1, enemyPosition2, enemyPosition3, enemyPosition4, enemyPosition5, enemyPosition6, enemyPosition7, enemyPosition8, enemyPosition9, enemyPosition10, enemyPosition11, enemyPosition12, enemyPosition13, enemyPosition14, enemyPosition15, enemyPosition16, enemyPosition17, enemyPosition18, enemyPosition19, enemyPosition20, enemyPosition21, enemyPosition22, enemyPosition23, enemyPosition24]
+
+    const moveEnemy = () => {
+
+        // create enemy
+
+        // first position
+
+        const move = (num, num2, arr) => {
+
+            enemyPosition1.forEach(element => {
+                var el = document.getElementById(element)
+                el.classList.add('enemy')
+            })
+
+            arr.forEach(x => {
+                setTimeout(() => {
+                    x.forEach(element => {
+                        var el = document.getElementById(element)
+                        el.classList.add('enemy')
+                    })
+                }, num)
+
+                num = num + 500
+            })
+
+            arr.forEach(x => {
+                setTimeout(() => {
+                    x.forEach(element => {
+                        var el = document.getElementById(element)
+                        el.classList.remove('enemy')
+                    })
+                }, num2)
+
+                num2 = num2 + 500
+            })
+        }
+
+        move(0, 500, enemyMoves)
+
+    }
+
+    const checkArray = (arr, value) => {
+        console.log(arr)
+        arr = arr.filter((n) => {return n != value})
+    }
+
+
+    moveEnemy()
+
     // Move hero left or right
 
     document.addEventListener('keydown', (event) => {
@@ -53,6 +130,15 @@ const startGame = () => {
         }
     }
 
+    const checkIfEnemyHasBeenShot = (el) => {
+        if (el.classList.contains('shot') && el.classList.contains('enemy')) {
+            el.classList.remove('enemy')
+            enemyMoves.forEach(arr => {
+                checkArray(arr, el)
+            })
+        }
+    }
+
     // Fire a shot
 
     const fireAShot = () => {
@@ -65,6 +151,8 @@ const startGame = () => {
                 document.getElementById(shooterStartingPoint).classList.remove('shot')
                 document.getElementById(shooterStartingPoint - 11).classList.add('shot')
                 shooterStartingPoint = shooterStartingPoint - 11
+
+                checkIfEnemyHasBeenShot(document.getElementById(shooterStartingPoint))
 
                 if (shooterStartingPoint < 11) {
                     clearInterval(shotDirection)
